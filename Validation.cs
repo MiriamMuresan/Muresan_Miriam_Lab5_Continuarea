18dcf3ce0e9cf7d488e582358f6c23267d3943a3
Raluca Miriam Muresan <miriammuresan27@yahoo.ro>
2021-11-18 15:23:15 +0200

    Am realizat laboratorul 7

---
+++ Validation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Muresan_Miriam_Lab5
+{
+    //validator pentru camp required
+    public class StringNotEmpty : ValidationRule
+    {
+        //mostenim din clasa ValidationRule
+        //suprascriem metoda Validate ce returneaza un ValidationResult
+        public override ValidationResult Validate(object value,System.Globalization.CultureInfo cultureinfo)
+        {
+            string aString = value.ToString();
+            if (aString == "")
+                return new ValidationResult(false, "String cannot be empty");
+            return new ValidationResult(true, null);
+        }
+    }
+    //validator pentru lungime minima a string-ului
+    public class StringMinLength : ValidationRule
+    {
+        public override ValidationResult Validate(object value,System.Globalization.CultureInfo cultureinfo)
+        {
+            string aString = value.ToString();
+            if (aString.Length < 3)
+                return new ValidationResult(false, "String must have at least 3 characters!");
+            return new ValidationResult(true, null);
+        }
+    }
+
+    public class StringMinimLength : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureinfo)
+        {
+            string aString = value.ToString();
+            if (aString.Length < 2)
+                return new ValidationResult(false, "String must have at least 2 characters!");
+            return new ValidationResult(true, null);
+        }
+    }
+
+    public class StringMaximLength : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureinfo)
+        {
+            string aString = value.ToString();
+            if (aString.Length > 1)
+                return new ValidationResult(false, "String must have  maximum 1 characters!");
+            return new ValidationResult(true, null);
+        }
+    }
+}
